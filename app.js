171cf485c706044fcf0b9517baec674bc8566740
MdMuzaffar <54287360+MdMuzaffar@users.noreply.github.com>
2023-10-02 23:53:14 +0530

    TODO App in Javascript

--- app.js
+++ app.js
@@ -9,3 +9,55 @@ window.addEventListener('click', () => {
         })
     })
 })
+
+const inputValue = document.getElementById('inputValue');
+const addTask = document.getElementById('addTask');
+const listTask = document.getElementById('listTask');
+
+addTask.addEventListener('click', () => {
+    const inputText = inputValue.value.trim();
+    if (inputText !== '') {
+        const taskItems = document.createElement('li');
+        taskItems.innerHTML = `<input type="checkbox">
+                                <span> ${inputText}</span>
+                                <button> Delete</button>`;
+    }
+
+
+});
+
+listTask.addEventListener('click', (event) => {
+    if (event.target.tagName === 'BUTTON') {
+        event.target.parentElement.remove();
+    } else if (event.target.tagName === 'INPUT') {
+        event.target.nextElementSibling.classList.toogle('complete');
+    }
+});
+
+const taskInput = document.getElementById('taskInput');
+const addTaskButton = document.getElementById('addTask');
+const taskList = document.getElementById('taskList');
+
+addTaskButton.addEventListener('click', () => {
+    const taskText = taskInput.value.trim();
+    if (taskText !== '') {
+        const taskItem = document.createElement('li');
+        taskItem.innerHTML = `
+                    <input type="checkbox">
+                    <span>${taskText}</span>
+                    <button>Delete</button>
+                `;
+
+        taskList.appendChild(taskItem);
+
+        taskInput.value = ''; // Clear the input field
+    }
+});
+
+taskList.addEventListener('click', (event) => {
+    if (event.target.tagName === 'BUTTON') {
+        event.target.parentElement.remove(); // Remove the task when the delete button is clicked
+    } else if (event.target.tagName === 'INPUT') {
+        event.target.nextElementSibling.classList.toggle('completed'); // Toggle the "completed" class
+    }
+});
